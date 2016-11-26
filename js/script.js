5c4cbcd664e91cdc6463c91fafdc4115d2380b29
Vladimir Trukhin <vvt@narod.ru>
2016-11-26 20:43:25 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ $(function() {
             center: location,
             zoom: 12,
             panControl: false,
+            scrollwheel: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         var map = new google.maps.Map(mapCanvas, mapOptions);
